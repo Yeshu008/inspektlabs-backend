689044736c67dc51239e8c1f1fca1b2f4be9665d
Yeshwanth C <yeshwanthshetty2002@gmail.com>
2025-07-29 22:56:22 +0530

    changes

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-echo "Waiting for MySQL to be ready..."
+MYSQL_HOST="${MYSQL_HOST:-db}"  # default to 'db' if not set
+MYSQL_PORT="${MYSQL_PORT:-3306}"
 
-until nc -z "$MYSQL_HOST" 3306; do
+echo "Waiting for MySQL at $MYSQL_HOST:$MYSQL_PORT..."
+
+until nc -z "$MYSQL_HOST" "$MYSQL_PORT"; do
   echo "MySQL is unavailable - sleeping"
   sleep 1
 done
 
-echo "MySQL is up - executing tests"
+echo "MySQL is up - continuing..."
 exec "$@"
