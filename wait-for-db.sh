c0e94f346d96fb0045a438c59ab3b603c168ee93
Yeshwanth C <yeshwanthshetty2002@gmail.com>
2025-07-29 22:50:59 +0530

    changes

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo "⏳ Waiting for MySQL to be ready..."
+echo "Waiting for MySQL to be ready..."
 
-until mysql -h db -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1;" "$MYSQL_DATABASE"; do
-  >&2 echo "MySQL is unavailable - sleeping"
-  sleep 2
+until nc -z "$MYSQL_HOST" 3306; do
+  echo "MySQL is unavailable - sleeping"
+  sleep 1
 done
 
-echo "✅ MySQL is up - continuing..."
+echo "MySQL is up - executing tests"
+exec "$@"
